b6008b9e555c55250f954f8d2be6f74cb734c2f4
Felipe Guimarães <felipeguimaraes2406@gmail.com>
2021-08-14 00:16:51 -0300

    create function to render table view cells

---
+++ InnovationTasks/Controllers/Cell/TaskTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TaskTableViewCell.swift
+//  InnovationTasks
+//
+//  Created by Felipe Guimarães on 11/08/21.
+//
+
+import UIKit
+
+class TaskTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var hourLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var taskDescriptionLabel: UILabel!
+    @IBOutlet weak var categoryView: UIView!
+}
--- InnovationTasks/Controllers/TasksTableViewController.swift
+++ InnovationTasks/Controllers/TasksTableViewController.swift
@@ -7,9 +7,39 @@
 
 import UIKit
 
+
+let category = Category(name: "Shopping List", color: UIColor.green)
+let tasks: [Task] = [
+    Task(name: "Create presentation for tomorrow", date: Date(), category: category),
+    Task(name: "This is my shopping list", date: Date(), category: category),
+]
+
 class TasksTableViewController: UITableViewController {
 
+    private var dateFormatter: DateFormatter = DateFormatter()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskCell", for: indexPath) as! TaskTableViewCell
+        let task = tasks[indexPath.row]
+        
+        dateFormatter.dateFormat = "HH:mm"
+        cell.hourLabel.text = dateFormatter.string(from: task.date)
+        
+        dateFormatter.dateFormat = "dd/MM/yyyy"
+        cell.dateLabel.text = dateFormatter.string(from: task.date)
+        
+        cell.categoryLabel.text = task.category.name
+        cell.categoryView.backgroundColor = task.category.color
+        cell.taskDescriptionLabel.text = task.name
+        
+        return cell
     }
 }
